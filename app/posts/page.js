25598925ff5e83e291a83b05c8e6dfd79cf302f1
Elias El Helou <elias.l.elhelou@gmail.com>
2025-02-11 18:04:56 +0200

    post 1

--- app/posts/page.js
+++ app/posts/page.js
@@ -2,6 +2,7 @@ export default function Postpone(){
     return(
         <div>
             <h1 className="text-3xl font-bold underline">Posts</h1>
+            <h2>post 1</h2>
         </div>
     )
 }
